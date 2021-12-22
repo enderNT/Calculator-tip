1eac5ebb6ad0e877f88af3b774882bb826a878f7
enderNT12 <gayagocr@gmail.com>
2021-12-21 19:13:35 -0600

    Resonsive añadido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import iconDollar from './images/icon-dollar.svg'
+import iconPerson from './images/icon-person.svg'
+import logo from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <img src={logo} alt='logo' className='Logo' />
+
+      <div className='Calculadora'>
+
+        <div className='Pay'>
+          <label for='Bill' className='LabelBill'>Bill</label>
+          <main className='BillAmount'>
+            <img src={iconDollar} alt='usd-icon' className='UsdIcon' />
+            <input type='number' className='Bill' name='Bill' />
+          </main>
+
+          <label className='LabelTips'>Select Tip %</label>
+          <main className='Tips'>
+            <button className='BotonTip'>5%</button>
+            <button className='BotonTip'>10%</button>
+            <button className='BotonTip'>15%</button>
+            <button className='BotonTip'>25%</button>
+            <button className='BotonTip'>50%</button>
+            <button className='BotonTip'>Custom</button>
+          </main>
+
+          <label for='NumberPeople' className='LabelPeople'>Number of People</label>
+          <main className='NumberPeople'>
+            <img src={iconPerson} alt='people-icon' className='PeopleIcon' />
+            <input type='number' className='People' name='People' />
+          </main>
+        </div>
+
+        <div className='TipBill'>
+
+          <main className='TipAmount'>
+            <div className='TextTip'>
+            <label>Tip Amount</label>
+            <h6 className='personTip'>  /person </h6>
+            </div>
+            <span>TIP AMOUNT</span>
+          </main>
+
+          <main className='Total'>
+            <div className='TextTotal'>
+            <label>Total</label>
+            <h6> /person </h6>
+            </div>
+            <span>TOTAL</span>
+          </main>
+          
+          <button className='BotonReset'>Reset</button>
+
+        </div>
+
+      </div>
+
+
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Gabriel</a>.
+      </footer>
     </div>
   );
 }
